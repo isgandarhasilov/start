57125414738118df5e55f3005d02d894f0a77c00
isgandarhasilov <isgandar.hasilov@yahoo.com>
2023-09-10 10:57:06 +0400

    practice js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+// this code works the modern way
 
 let value = true;
 alert(typeof value); // boolean
@@ -7,3 +8,11 @@ value = String(value); // now value is a string "true"
 alert(typeof value); // string
 
 
+let arr = ["I", "go", "home"];
+
+delete arr[1]; // remove "go"
+
+alert( arr[1] ); // undefined
+
+// now arr = ["I",  , "home"];
+alert( arr.length ); // 3
